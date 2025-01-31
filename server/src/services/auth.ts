9051d896363557dad309fce9d8bf4b1e24d3a16a
Colton Doling <cmdoling@gmail.com>
2025-01-31 14:12:39 -0500

    updated authenticateToken function

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,4 +1,3 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { GraphQLError } from 'graphql';
 
@@ -11,24 +10,24 @@ interface JwtPayload {
   email: string,
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = ({ req }: any) => {
   const authHeader = req.headers.authorization;
-
+  
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-
+    
     const secretKey = process.env.JWT_SECRET_KEY || '';
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
+    try {
+      const { data }: any = jwt.verify(token, secretKey);
+      req.user = data as JwtPayload;
+    } catch (err) {
+      console.log('Invalid token');
+    }
+    
+    return req;
   } else {
-    res.sendStatus(401); // Unauthorized
+    return req;
   }
 };
 

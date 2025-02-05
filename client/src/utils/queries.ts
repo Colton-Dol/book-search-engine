06c813e8a31ed8bca968ca981a08fcb7651a8dc3
Colton Doling <cmdoling@gmail.com>
2025-02-04 20:42:04 -0500

    update me query to just return username and saved books

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -3,9 +3,7 @@ import { gql } from '@apollo/client';
 export const QUERY_ME = gql`
     query me {
         me {
-            _id
             username
-            bookCount
             savedBooks {
                 bookId
                 authors

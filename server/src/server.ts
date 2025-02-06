b5f42cabf7038bbc744376b58f2c7ac7873dfac6
Colton Doling <cmdoling@gmail.com>
2025-02-06 15:08:14 -0500

    fixed pathing for static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,7 +31,7 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(`${__dirname}../client/dist`));
+    app.use(express.static(`${__dirname}/client/dist`));
   }
 
   app.listen(PORT, () => {
